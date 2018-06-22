dfdc944ee9dea82270f47cdf1f43a0bb91e66ffd
Milan Nemec <milan.nemec@apollogames.com>
2018-06-22 12:42:44 +0200

    simplified grid for input

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,23 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import Handsontable from 'handsontable';
 import { HotTable } from '@handsontable/react';
 
 export default class App extends React.Component {
   constructor(props) {
     super(props);
-    this.data = [
-      ["", "Ford", "Volvo", "Toyota", "Honda"],
-      ["2016", 10, 11, 12, 13],
-      ["2017", 20, 11, 14, 13],
-      ["2018", 30, 15, 12, 13]
-    ];
+    this.data = Handsontable.helper.createSpreadsheetData(100, 2);
+    
+    this.colHeaders = ['White', 'Black'];
+    this.contextMenu = ['row_above', 'row_below'];
   }
 
   render() {
     return (
        <div id="hot-app">
-        <HotTable data={this.data} colHeaders={true} rowHeaders={true} width="600" height="300" stretchH="all" />
+        <HotTable data={this.handsontableData} colHeaders={true} rowHeaders={true} width="200"  stretchH="all" colHeaders={this.colHeaders} contextMenu={this.contextMenu}  
+        allowInsertColumn={false} maxCols={2} minCols={2} minRows={1} minSpareRows={1} startRows={1}/>
       </div>
     );
   }
-}
+} 
