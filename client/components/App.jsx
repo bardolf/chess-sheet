1225aaff0e7852ead44929e15b4b3dc1011dddb4
Milan Nemec <milan.nemec@apollogames.com>
2018-06-22 10:02:13 +0200

    handsontable added

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,10 +1,23 @@
 import React from 'react';
+import ReactDOM from 'react-dom';
+import { HotTable } from '@handsontable/react';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.data = [
+      ["", "Ford", "Volvo", "Toyota", "Honda"],
+      ["2016", 10, 11, 12, 13],
+      ["2017", 20, 11, 14, 13],
+      ["2018", 30, 15, 12, 13]
+    ];
+  }
+
   render() {
     return (
-     <div style={{textAlign: 'center'}}>
-        <h1>Hello World</h1>
-      </div>);
+       <div id="hot-app">
+        <HotTable data={this.data} colHeaders={true} rowHeaders={true} width="600" height="300" stretchH="all" />
+      </div>
+    );
   }
 }
