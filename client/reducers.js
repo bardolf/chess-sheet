fb81458668e7cc60921007ee4023d7971da2fd48
Milan Nemec <milanemec@gmail.com>
2018-06-29 15:56:23 +0200

    czech transposition improved

--- client/reducers.js
+++ client/reducers.js
@@ -32,7 +32,7 @@ const analyze = data => {
     for (var row = 0; row < data.length; row++) {
         for (var col = 0; col < data[row].length; col++) {
             var move = data[row][col];
-            move = czech2English(move);
+            move = czech2English(move, chess);
             if (errorMessage !== null) {
                 if (!errorConfirmed && (move !== null && move.trim().length != 0)) {
                     errorConfirmed = true;
@@ -60,20 +60,32 @@ const errorMessageText = (row, col) => {
     return 'Neplatný ' + (row + 1) + '. tah ' + (col == 0 ? 'bílého' : 'černého');
 }
 
-const czech2English = move => {
+const czech2English = (move, chess) => {
     if (move === null || move.trim().length == 0) {
         return move;
     }
+    // debugger;
+    //let's avoid the ambiguity with queen/dama and d pawn 
+    if (/^[d]\d$/.test(move) || /^[d]x.\d$/.test(move)) { 
+        //it starts with the 'd', let's check, whether it could be a pawn
+        if (chess.move(move)) {
+            //it is valid, let's keep it
+            chess.undo();
+        } else {
+            move = move.replace(/^[d]/, "D");
+        }
+    }
+
     //transform czech notation to english one - pieces moves
-    if (/^[jJsSvVdDk].\d$/.test(move) || /^[jJsSvVdDk]..\d$/.test(move)) {
+    if (/^[jJsSvVDdk].\d$/.test(move) || /^[jJsSvV]..\d$/.test(move) || /^[jJsSvV].x.\d$/.test(move) || /^[Dk]x.\d$/.test(move)) {
         move = move.replace(/^[jJ]/, "N");
         move = move.replace(/^[sS]/, "B");
         move = move.replace(/^[vV]/, "R");
-        move = move.replace(/^[dD]/, "Q");
+        move = move.replace(/^[Dd]/, "Q");
         move = move.replace(/^[k]/, "K");
     }
     move = move.replace(/^[oO]-[oO]$/, "O-O");
-    move = move.replace(/^[oO]-[oO]-[oO]$/, "O-O-O");
+    move = move.replace(/^[oO]-[oO]-[oO]$/, "O-O-O");    
 
     if (/^.8[jJsSvVdD]$/.test(move)) {
         move = move.replace(/[jJ]$/, "N");
